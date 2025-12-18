f1ca7bc64e9589761cbaecb9896128cdfb00de2a
JMC <jmc8@qq.com>
2025-12-18 20:05:00 +0800

    调整patch

--- Patches/ModEntryPatch.cs
+++ Patches/ModEntryPatch.cs
@@ -1,7 +1,9 @@
 ﻿using BetterModManager.UI;
+using BetterModManager.Utils;
 using Duckov.Modding;
 using Duckov.Modding.UI;
 using HarmonyLib;
+using JmcModLib.Reflection;
 using JmcModLib.Utils;
 
 namespace BetterModManager.Patches
@@ -18,7 +20,7 @@ namespace BetterModManager.Patches
             ComponentHelper.AddComponentIfNeeded<UI.ModEntryDragHandler>(__instance.gameObject, handler => handler.Setup(__instance, master), "ModEntryDragHandler 已添加并初始化");
 
 
-            ComponentHelper.AddComponentIfNeeded<UI.ModEntryLockHandler>(
+            ComponentHelper.AddComponentAlways<UI.ModEntryLockHandler>(
                 __instance.gameObject,
                 handler => handler.Setup(__instance, (locked) => {
                     ModLogger.Debug($"回调触发：锁定状态 = {locked}");
@@ -26,11 +28,33 @@ namespace BetterModManager.Patches
                 "ModEntryLockHandler 已添加并初始化"
             );
 
-            ComponentHelper.AddComponentAlways<UI.ModEntryKeyController>(__instance.gameObject, handler => handler.Setup(__instance, index), "ModEntryKeyController 已添加并初始化");
-            if (ModEntryKeyController.preIdx != null)
-            {
-                ModEntryKeyController.isListening = true;
-            }
+            ComponentHelper.AddComponentAlways<UI.ModEntryPinHandler>(__instance.gameObject, initHandler => initHandler.Setup(__instance), "ModEntryPinHandler 已初始化");
+
+            ComponentHelper.AddComponentAlways<UI.ModEntryKeyController>(__instance.gameObject, handler => handler.Setup(__instance, modInfo, index), "ModEntryKeyController 已添加并初始化");
+        }
+
+        // 拦截“上移”按钮
+        [HarmonyPrefix]
+        [HarmonyPatch("OnButtonReorderUpClicked")]
+        private static bool PrefixOnUpClicked(ModEntry __instance)
+        {
+            // 获取当前的 index
+            int index = MemberAccessor.Get(typeof(ModEntry), "index").GetValue<ModEntry, int>(__instance);
+            ReorderHelper.Inc(index);
+
+            // 返回 false 阻止原版方法运行
+            return false;
+        }
+
+        // 拦截“下移”按钮
+        [HarmonyPrefix]
+        [HarmonyPatch("OnButtonReorderDownClicked")]
+        private static bool PrefixOnDownClicked(ModEntry __instance)
+        {
+            int index = MemberAccessor.Get(typeof(ModEntry), "index").GetValue<ModEntry, int>(__instance);
+            ReorderHelper.Dec(index);
+
+            return false;
         }
     }
 }
